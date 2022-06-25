1ce1f4c1aa9e4afe3f565a5f9471bf7f50143eba
Veera Bhargava Reddy. Obulareddy <obulareddyveera@gmail.com>
2022-06-25 12:27:25 +0530

    GET - /api/category development completed

---
+++ pages/api/category.js
@@ -0,0 +1,20 @@
+import prisma from '../../prisma/client';
+
+export default async function categoryHandler(req, res) {
+    const { method } = req;
+
+    switch (method) {
+        case "GET":
+            try {   
+                const data = await prisma.category.findMany();
+                res.status(200).json(data);
+            } catch(e) {
+                res.status(500).json({ error: "Error fetching category" });
+            }
+            break;
+        default:
+            res.setHeader("Allow", ["GET"]);
+            res.status(405).end(`Method ${method} Not Allowed`);
+            break;
+    }
+};
